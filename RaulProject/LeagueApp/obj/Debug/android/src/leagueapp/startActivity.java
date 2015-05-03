d534ab641249c3d033b4e493df43ba7d19c6eeb8
BarryLendertz <lendertz@live.fr>
2015-05-03 21:09:28 +0200

    StartCommit

---
+++ RaulProject/LeagueApp/obj/Debug/android/src/leagueapp/startActivity.java
@@ -0,0 +1,46 @@
+package leagueapp;
+
+
+public class startActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("LeagueApp.startActivity, LeagueApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", startActivity.class, __md_methods);
+	}
+
+
+	public startActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == startActivity.class)
+			mono.android.TypeManager.Activate ("LeagueApp.startActivity, LeagueApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ RaulProject/LeagueApp/startActivity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace LeagueApp
+{
+	[Activity (Label = "LeagueApp", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+	public class startActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// set the layout for this activity
+			SetContentView (Resource.Layout.startActivity);
+
+			// establish the elements in the layout
+			Button proceedButton = FindViewById<Button> (Resource.Id.proceedButton);
+			EditText editText1 = FindViewById<EditText> (Resource.Id.editText1);
+			TextView textView1 = FindViewById<TextView> (Resource.Id.textView1);
+			TextView textView2 = FindViewById<TextView> (Resource.Id.textView2);
+			TextView textView3 = FindViewById<TextView> (Resource.Id.textView3);
+			CheckBox checkBox1 = FindViewById<CheckBox> (Resource.Id.rememberMe);
+
+			
+			proceedButton.Click += delegate {
+				// check if everything's filled
+				string Region = "";
+				string SummonerName = editText1.Text;
+				bool rememberMe = checkBox1.Checked;
+
+				RadioGroup radioGroup1 = FindViewById<RadioGroup> (Resource.Id.radioGroup1);
+
+				RadioButton radioButton1 = FindViewById<RadioButton> (Resource.Id.radioButton1);
+				RadioButton radioButton2 = FindViewById<RadioButton> (Resource.Id.radioButton2);
+				RadioButton radioButton3 = FindViewById<RadioButton> (Resource.Id.radioButton3);	
+
+
+				List<RadioButton>  radioButtons = new List<RadioButton>();
+
+				radioButtons.Add(radioButton1);
+				radioButtons.Add(radioButton2);
+				radioButtons.Add(radioButton3);
+			
+			
+				// Run a loop to check if and which RadioButton is selected;
+				for (int checkedID = 0; checkedID <= radioButtons.Count - 1; checkedID++) {
+					if (radioButtons[checkedID].Checked){
+						Region = radioButtons[checkedID].Text;
+					};
+				};
+
+
+				// Check if the summoner name and region is valid.
+				if (SummonerName.Length <= 1) 
+				{
+					Toast.MakeText(this, "You didn't fill out a valid summoner name!", ToastLength.Short).Show();
+					return;
+				}
+				else if (Region.Length == 0)
+				{
+					Toast.MakeText(this, "You didn't select a region!", ToastLength.Short).Show();
+					return;
+				};
+			};
+		}
+	}
+}
+
+
